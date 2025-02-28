5ee3544d78c067214db6a670c2c3d71abbb00874
Batataw <batataw@gmail.com>
2025-02-28 11:56:43 +0100

    Initial Commit

---
+++ CineBot/CineBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineBotApp.swift
+//  CineBot
+//
+//  Created by Alan Digital on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CineBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineBotTests/CineBotTests.swift
@@ -0,0 +1,17 @@
+//
+//  CineBotTests.swift
+//  CineBotTests
+//
+//  Created by Alan Digital on 28/02/2025.
+//
+
+import Testing
+@testable import CineBot
+
+struct CineBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
